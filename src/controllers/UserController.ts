61749a3823eed1786ace353191b584d88e9af9fa
Matheus Palmieri <matheusandrepp@gmail.com>
2022-11-09 00:02:18 -0300

    EmailService

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import EmailService from "../services/EmailService";
 
 const users = [
   {
@@ -10,5 +11,21 @@ const users = [
 export default {
   async index(req: Request, res: Response) {
     return res.json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendMail({
+      to: {
+        name: "Matheus André Palmieri",
+        email: "matheuspalmieri@gmail.com",
+      },
+      message: {
+        subject: "Bem-vindo ao sistema",
+        body: "Seja bem-vindo",
+      },
+    });
+    return res.send();
   },
 };
